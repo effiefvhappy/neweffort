64c516e90a23728d4f5b09b8b44c482408fc1c2d
renjin <00317574@163.com>
2019-03-26 23:42:59 +0800

    init

---
+++ src/main/java/algorithm/HeapSort.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+/**
+ *
+ * @author mac
+ */
+public class HeapSort {
+
+    private  int[] arrays=null;
+
+    public HeapSort(int[] arrays){
+        this.arrays=arrays;
+    }
+
+    public static void main(String[] args) {
+        int[] arras={9,1,5,8,3,7,4,6,2};
+        HeapSort heapSort=new HeapSort(arras);
+    }
+
+    public  void heapSort(int[] arrays){
+
+    }
+
+    public  void creatHeapSort(int start,int end){
+        this.arrays[0]=1;
+
+    }
+
+
+
+}
